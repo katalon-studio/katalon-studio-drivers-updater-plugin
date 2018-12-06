97ad586abaf8aa4662885b02bdc0b40e00b73834
tuananht <tuananhtran@kms-technology.com>
2018-12-06 17:20:18 +0700

    writeToJson

--- src/main/java/com/katalon/plugin/drivers_updater/Contents.java
+++ src/main/java/com/katalon/plugin/drivers_updater/Contents.java
@@ -10,21 +10,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Contents {
     @XmlElement(name = "Key")
     private String Key;
-//
-//    @XmlElement(name = "Generation")
-//    private String Generation;
-//
-//    @XmlElement(name = "MetaGeneration")
-//    private String MetaGeneration;
-//
-//    @XmlElement(name = "LastModified")
-//    private String LastModified;
-//
-//    @XmlElement(name = "ETag")
-//    private String ETag;
-//
-//    @XmlElement(name = "Size")
-//    private String Size;
 
     public String getKey() {
         return Key;
@@ -34,51 +19,11 @@ public class Contents {
         Key = key;
     }
 
-//    public String getGeneration() {
-//        return Generation;
-//    }
-//
-//    public void setGeneration(String generation) {
-//        Generation = generation;
-//    }
-//
-//    public String getMetaGeneration() {
-//        return MetaGeneration;
-//    }
-//
-//    public void setMetaGeneration(String metaGeneration) {
-//        MetaGeneration = metaGeneration;
-//    }
-//
-//    public String getLastModified() {
-//        return LastModified;
-//    }
-//
-//    public void setLastModified(String lastModified) {
-//        LastModified = lastModified;
-//    }
-//
-//    public String getETag() {
-//        return ETag;
-//    }
-//
-//    public void setETag(String etag) {
-//        ETag = etag;
-//    }
-//
-//    public String getSize() {
-//        return Size;
-//    }
-//
-//    public void setSize(String size) {
-//        Size = size;
-//    }
 
     @Override
     public String toString() {
 
-        return this.Key;// + " "  + this.Generation + " " + this.MetaGeneration + " " +
-//                this.LastModified + "  " + this.ETag + " " + this.Size;
+        return this.Key;
     }
 
 }
--- src/main/java/com/katalon/plugin/drivers_updater/ListBucketResult.java
+++ src/main/java/com/katalon/plugin/drivers_updater/ListBucketResult.java
@@ -1,28 +1,13 @@
 package com.katalon.plugin.drivers_updater;
 
-import com.sun.xml.internal.txw2.annotation.XmlNamespace;
-
 import javax.xml.bind.annotation.*;
 import java.util.List;
 
 @XmlRootElement(name = "ListBucketResult")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ListBucketResult {
-//    @XmlElement(name = "Name")
-//    private String Name;
-//
-//    @XmlElement(name = "Prefix")
-//    private String Prefix;
-//
-//    @XmlElement(name = "Marker")
-//    private String Marker;
-//
-//    @XmlElement(name = "IsTruncated")
-//    private String IsTruncated;
-
     @XmlElement(name = "Contents")
     private List<Contents> Contents;
-
 
     public List<Contents> getContents() {
         return Contents;
